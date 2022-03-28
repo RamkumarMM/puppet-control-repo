489c9f94290f1e5412225fc26c691aa4b49f4d85
ram <ram@kube-master.lab.net>
2022-03-28 14:02:47 +0000

    creating PROD environment

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,5 +3,5 @@ File { backup => false }
 ## DEFAULT NODE CLASSIFICATION ##
 node default {
   #include  ucm_roles::iaas
-  notify { "Test message from DEV environment": }
+  notify { "Test message from PROD environment": }
 }
